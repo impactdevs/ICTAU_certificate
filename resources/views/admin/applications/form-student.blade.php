2ee080323468b3fe6fc2c06bd18e06f7607675f9
Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
2024-06-04 09:08:56 +0300

    bio-data form update

--- resources/views/admin/applications/form-student.blade.php
+++ resources/views/admin/applications/form-student.blade.php
@@ -1,12 +1,15 @@
 <x-forms.input name="title" label="Title" type="text" id="title" placeholder="Your title eg. Mr., Mrs, Prof" />
 <x-forms.input name="first_name" label="First Name" type="text" id="first_name" placeholder="Enter your first name" />
 <x-forms.input name="last_name" label="Last Name" type="text" id="last_name" placeholder="Enter your last name" />
-<x-forms.input name="phone_number" label="Phone Number" type="tel" id="phone_number" placeholder="Enter your phone number eg 0786065399" />
+<x-forms.input name="phone_number" label="Phone Number" type="tel" id="phone_number"
+    placeholder="Enter your phone number eg 0786065399" />
 <x-forms.input name="date_of_birth" label="Date of Birth" type="date" id="date_of_birth" />
 <x-forms.input name="email" label="Email" type="email" id="email" placeholder="Enter your email" />
 <x-forms.upload name="curriculum_vitae" label="Curriculum Vitae" id="curriculum_vitae" />
-<x-forms.input name="institution_name" label="Title (Name of Institution)" type="text" id="institution_name" placeholder="Enter your institution name" />
-<x-forms.input name="course" label="Course" type="text" id="course" placeholder="Enter the course you are pursuing" />
+<x-forms.input name="institution_name" label="Title (Name of Institution)" type="text" id="institution_name"
+    placeholder="Enter your institution name" />
+<x-forms.input name="course" label="Course" type="text" id="course"
+    placeholder="Enter the course you are pursuing" />
 <x-forms.upload name="passport_photo" label="Passport Photo" id="passport_photo" />
 <x-forms.upload name="payment_proof" label="Payment Proof" id="payment_proof" />
 <div class="form-group">
