9d2aff7e1adbec13aa210f591db6853fae4dcfce
Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
2025-01-11 19:03:34 +0300

    new update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Member;
+use App\Models\Applicant;
 
 class DashboardController extends Controller
 {
     public function index() {
         $memberships = Member::count();
-        return view('dashboard', compact('memberships'));
+        $new_applications = Applicant::where('application_status', 'pending')->count();
+        return view('dashboard', compact('memberships', 'new_applications'));
     }
 }
